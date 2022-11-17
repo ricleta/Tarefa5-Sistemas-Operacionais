2b2f7603958259c4a44d36a0e66108f2b3e0571b
Eduardo Dantas Luna <75355609+MichaelTheFear@users.noreply.github.com>
2022-11-17 01:44:43 +0000

    Commit test replit

--- rascunho/simulador.c
+++ rascunho/simulador.c
@@ -2,41 +2,102 @@
 
 int main(int argc, char * argv[])
 {
+  int page_size, mem_size, n_pages, page_id, n_page_faults, page_ids_size;
+  int alg_type = -1; // -1 -> invalido, 0 -> LRU, 1 -> NRU
+  Page *pages;
+  int *page_ids;
+  int time = 0;
+  FILE *arq;
+  unsigned int addr;
+  char rw;
+  
   if (argc < 4)
   {
     puts("4 argumentos necessarios");
     exit(0);
   }
 
-  int n_paginas = 0;
-  MEM *log_addr;
-  
-  // printf("./simulador %s %s %s %s\n", argv[1], argv[2], argv[3], argv[4]);
   if (strcmp(argv[1], "LRU") == 0)
   {
-    log_addr = (MEM *) malloc(sizeof(MEM) * n_paginas);
-    printf("LRU\n");
+    alg_type = 0;
   }
-  else if (strcmp(argv[1], "NRU") == 0)
+
+  if (strcmp(argv[1], "NRU") == 0)
   {
-    log_addr = (MEM *) malloc(sizeof(MEM) * n_paginas);
-    printf("NRU\n");
+    alg_type = 1;
   }
   
-  FILE *arq = fopen(argv[2], "r");
-  unsigned int addr;
-  char rw;
-  int page = 0;
-  int s = 13; // para pag de 8k e mem fisica de 16 M
+  if (alg_type == -1)
+  {
+    perror("Algoritmo invalido, apenas LRU e NRU sao validos");
+    exit(1);
+  }
+  
+  arq = fopen(argv[2], "r");
+  
+  if (arq == NULL)
+  {
+    perror("arquivo de log nao encontrado");
+    exit(2);
+  }
+  
+  if (8 > atoi(argv[3]) || atoi(argv[3]) > 32)
+  {
+    perror("tamanho de pagina invalido, deve ser entre 8 e 32");
+    exit(3);
+  }
+
+  if (1 > atoi(argv[4]) || atoi(argv[4]) > 16)
+  {
+    perror("tamanho de memoria fisica invalido, deve ser entre 1 e 16");
+    exit(4);
+  }
+
+  arq = fopen(argv[2], "r");
+  page_size = atoi(argv[3]);
+  mem_size = atoi(argv[4]);
+  
+  n_pages = (page_size * 1000)/mem_size;
+
+  pages = (Page *) malloc(sizeof(Page) * n_pages);
+  //page_ids = (int *) malloc(sizeof(Page) * n_pages)
+  
+  if (pages == NULL)
+  {
+    perror("erro no malloc");
+    exit(5);
+  }
   
   while(fscanf(arq, "%x %c ", &addr, &rw) == 2)
-    {
-      page = addr >> s;
-      
-      // printf("%x\n", page);
-      
-      // printf("%x %c\n", addr, rw);
-    }
+  {
+      for (int i = 0; i < n_pages; i++)
+      {
+        page_id = indice_pagina(addr, page_size);
+        
+        if (pages[i].id == page_id)
+        {
+          pages[i].t_last_access = time;
+          time++;
+          break;
+        }
+        else
+        {
+          if (alg_type == 0)
+          {
+            lru(pages,page_ids,page_id,n_pages);
+          }
+          else
+          {
+            nru(pages,page_id);
+          }
+
+          time++;
+          n_page_faults++;
+          break;
+        }
+      }
+  }
   
   return 0;
 }
+
--- rascunho/simulador.h
+++ rascunho/simulador.h
@@ -1,15 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-typedef struct _mem
+typedef struct _page
 {
   int R;
   int M;
-  int last_access;
-} MEM;
+  int t_last_access;
+  int id;
+} Page;
 
-int equals(char *str1, char *str2)
+int indice_pagina(int endereco,int paginaTamanho)
 {
+  int res;
+  paginaTamanho *= 1000;
+  res = (int) ceil(log2(paginaTamanho));
+  return endereco >> res;
+}
+
+
+int lru(Page * pages,int page_id,int pages_size)
+{
+  int min_id = 0;
+  int min_t = pages->id;
   
+  for(int i = 0; i < pages_size ; i++){
+    
+  }
+}
+
+int nru(Page * pages,int page_id)
+{
+    
 }
