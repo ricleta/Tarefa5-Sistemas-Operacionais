5903702262a0ba6a837194f2ff8fd05bb406b401
ricleta <47068518+ricleta@users.noreply.github.com>
2022-11-18 02:21:13 +0000

    refatoracao e inicio implementacao NRU

--- rascunho/simulador.c
+++ rascunho/simulador.c
@@ -1,10 +1,15 @@
+// Eduardo Dantas Luna, 2111484
+// Ricardo Bastos Leta Vieira, 2110526
+
 #include "simulador.h"
 
 int main(int argc, char * argv[])
 {
   int page_size, mem_size, n_pages, page_id, n_page_faults, page_ids_size;
+  int index_to_overwrite;
   int alg_type = -1; // -1 -> invalido, 0 -> LRU, 1 -> NRU
   Page *pages;
+  int pages_current_size = 0;
   int *page_ids;
   int time = 0;
   FILE *arq;
@@ -25,6 +30,7 @@ int main(int argc, char * argv[])
   if (strcmp(argv[1], "NRU") == 0)
   {
     alg_type = 1;
+    page_ids = page_ids_arr(n_pages);
   }
   
   if (alg_type == -1)
@@ -56,12 +62,10 @@ int main(int argc, char * argv[])
   arq = fopen(argv[2], "r");
   page_size = atoi(argv[3]);
   mem_size = atoi(argv[4]);
-  int cycle_num =0;
   n_pages = (page_size * 1000)/ mem_size;
-  int pages_current_size = 0;
   pages = (Page *) malloc(sizeof(Page) * n_pages);
   //page_ids = (int *) malloc(sizeof(Page) * n_pages)
-  printf("Numero de Paginas em memoria max: %d\n",n_pages);
+  printf("Numero de max de paginas em memoria: %d\n",n_pages);
   
   if (pages == NULL)
   {
@@ -71,34 +75,40 @@ int main(int argc, char * argv[])
   
   while(fscanf(arq, "%x %c ", &addr, &rw) == 2)
   {
-      page_id = indice_pagina(addr, page_size);
+      page_id = get_indice_pagina(addr, page_size);
 
-      if(!onPages(pages,page_id,pages_current_size))
+      if(!is_in_Pages(pages,page_id,pages_current_size))
       {  
         if(pages_current_size >= n_pages)
         {
-            if (alg_type == 0)
-            {
-              int index_to_overwrite = lru(pages,page_id,n_pages);
-              setPageAt(pages,index_to_overwrite,page_id,cycle_num);
-              n_page_faults++;
-            }
-            else
-            {
-              //nru(pages,page_id);
-            }
-        } 
-        else 
-        {
-           setPageAt(pages,pages_current_size,page_id,cycle_num);
-          n_page_faults++;
-          pages_current_size++;
-        }
+          if (alg_type == 0)
+          {
+            index_to_overwrite = lru(pages,page_id,n_pages);
+            set_page(pages,index_to_overwrite,page_id,time, rw);
+            // page_ids[pages_current_size] = index_to_overwrite;
+            // n_page_faults++;
+            // pages_current_size++;
+          }
+          else if(alg_type == 1)
+          {
+            index_to_overwrite = nru(pages,page_ids,n_pages);
+            set_page(pages,index_to_overwrite,page_id,time, rw);
+            page_ids[pages_current_size] = index_to_overwrite;
+          }
+      } 
+      else 
+      {
+        set_page(pages,pages_current_size,page_id,time, rw);
+        page_ids[pages_current_size] = page_id;
+        pages_current_size++;
+      }
+        
+      n_page_faults++;
     }
-    cycle_num++;
+    time++;
   }
 
-  printf("Numero trocas: %d \n",n_page_faults);
+  printf("Page Faults: %d \n",n_page_faults);
   return 0;
 }
 
--- rascunho/simulador.h
+++ rascunho/simulador.h
@@ -1,3 +1,6 @@
+// Eduardo Dantas Luna, 2111484
+// Ricardo Bastos Leta Vieira, 2110526
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,13 +8,13 @@
 
 typedef struct _page
 {
-  int R;
-  int M;
+  int R; // 0 para false, 1 para true
+  int M; // 0 para false, 1 para true
   int t_last_access;
   int id;
 } Page;
 
-int indice_pagina(int endereco,int paginaTamanho)
+int get_indice_pagina(int endereco,int paginaTamanho)
 {
   int res;
   paginaTamanho *= 1000;
@@ -19,6 +22,57 @@ int indice_pagina(int endereco,int paginaTamanho)
   return endereco >> res;
 }
 
+int * page_ids_arr(int tam) 
+{
+	int *arr = (int *) malloc(sizeof(int) * tam);
+
+	for(int i = 0; i < tam; i++)
+  {
+    arr[i] = -1;      
+  }
+
+	return arr;
+}
+
+int is_in_Pages(Page * vector, int id,int len)
+{
+  if(len == 0)
+  {
+    return 0;
+  }
+  
+  for(int i = 0; i< len;i++)
+  {
+    if(vector[i].id==id)
+    {
+      return 1;
+    }
+  }
+  
+  return 0;
+}
+
+void set_page(Page * pages, int index, int id, int last_access, char RW)
+{
+  int R;
+  int M;
+
+  if(RW == 'R') 
+  {
+    R = 1;
+    M = 0;
+  }
+  else if(RW == 'W')
+  {
+    R = 1;
+    M = 1;    
+  }
+  
+  pages[index].id = id;
+  pages[index].t_last_access = last_access;
+  pages[index].M = M;
+  pages[index].R = R;
+}
 
 int lru(Page * pages,int page_id,int pages_size)
 {
@@ -38,34 +92,97 @@ int lru(Page * pages,int page_id,int pages_size)
   return min_id;
 }
 
-int nru(Page * pages,int page_id)
+int nru(Page * pages,int *page_ids, int pages_size)
 {
-  // TODO
-  return -1;
-}
-
-int onPages(Page * vector, int id,int len)
-{
-  if(len == 0)
-  {
-    return 0;
-  }
+  	int index_in_pages, flag, tam;
+    int * no_ref_no_mod; // array de paginas nao referenciadas e nao modificadas
+    int * mod_no_ref; // array de paginas nao referenciadas mas modificadas
+    int * ref_no_mod; // array de paginas referenciadas mas nao modificadas
+    int * ref_mod; // array de paginas referenciadas e modificadas
   
-  for(int i = 0; i< len;i++)
-  {
-    if(vector[i].id==id)
+  	int id_arr0 = 0; // indice atual de no_ref_no_mod
+    int id_arr1 = 0; // indice atual de mod_no_ref
+    int id_arr2 = 0; // indice atual de ref_no_mod
+    int id_arr3 = 0; // indice atual de ref_mod
+
+    // tempo e posição da página mais antiga
+  	int t_antiga; 
+    int pos_antiga = 0; 
+  	
+  	no_ref_no_mod = page_ids_arr(pages_size); 
+  	mod_no_ref = page_ids_arr(pages_size); 
+  	ref_no_mod = page_ids_arr(pages_size); 
+  	ref_mod = page_ids_arr(pages_size);
+  
+  	for (int i = 0; i < pages_size; i++) 
     {
-      return 1;
-    }
-  }
+  		index_in_pages = page_ids[i];
+  		if (pages[index_in_pages].R == 0) 
+      {
+  			if(pages[index_in_pages].M == 0) 
+        {
+  				//não referenciada e não modificada
+  				no_ref_no_mod[id_arr0] = i; // Armazena índice vp
+  				id_arr0++;
+  			}
+  			else 
+        {
+  				//não referenciada e modificada
+  				mod_no_ref[id_arr1] = i;
+  				id_arr1++;
+  			}
+  		} 
+  		else 
+      {
+  			if(pages[index_in_pages].M == 0) 
+        {
+  				//referenciada e não modificada
+  				ref_no_mod[id_arr2] = i;
+  				id_arr2++;
+  			}
+  			else 
+        {
+  				//referenciada e modificada
+  				ref_mod[id_arr3] = i;
+  				id_arr3++;
+  			}
+  		}
+  	}
   
-  return 0;
-}
-
-void setPageAt(Page * pages, int index, int id, int last_access)
-{
-  pages[index].id = id;
-  pages[index].t_last_access = last_access;
-  pages[index].M = -1;
-  pages[index].R = -1;
+  	int * vet;
+  	if ( no_ref_no_mod[0] != -1){
+  		vet = no_ref_no_mod;
+  		tam = id_arr0;
+  	} 
+  	else if (mod_no_ref[0] != -1) {
+  		vet = mod_no_ref;
+  		tam = id_arr1;
+  	} 
+  	else if (ref_no_mod[0] != -1) {
+  		vet = ref_no_mod;
+  		tam = id_arr2;
+  	} 
+  	else {
+  		vet = ref_mod;
+  		tam = id_arr3;
+  	}
+  	
+  	// vet contém todos os índices de vp de um dos critérios acima
+  	// caso exista mais de um elemento no vetor, tirar o mais antigo
+  	pos_antiga = 0;
+  	index_in_pages = page_ids[vet[0]];
+  	t_antiga = pages[index_in_pages].t_last_access;
+  	
+  	for(int i = 1; i < tam ; i++) 
+    {
+  		index_in_pages = page_ids[vet[i]];
+  		
+      if (t_antiga > pages[index_in_pages].t_last_access) 
+      {
+  			pos_antiga = vet[i];
+  			t_antiga = pages[index_in_pages].t_last_access;
+  		}
+  	}
+  
+  return pos_antiga;
 }
